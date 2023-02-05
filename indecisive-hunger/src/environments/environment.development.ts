b844e00204eb9bc2d297741cde1ef9aa69cde28a
Nicky Kayyarath <nkayyarath@gmail.com>
2023-02-05 16:39:19 -0500

    angular project created and connected to firebase

---
+++ indecisive-hunger/src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'indecisive-hunger',
+    appId: '1:97921916722:web:c9891466e17106daef812b',
+    databaseURL: 'https://indecisive-hunger-default-rtdb.firebaseio.com',
+    storageBucket: 'indecisive-hunger.appspot.com',
+    apiKey: 'AIzaSyAwwTOfsj4DOR9998J-tT4w9ywQQIs354Y',
+    authDomain: 'indecisive-hunger.firebaseapp.com',
+    messagingSenderId: '97921916722',
+    measurementId: 'G-CRXX0KCJE5',
+  },
+    production: false,
+}
